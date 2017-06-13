86e9e81e2cc278ef0dd7bce103b3746100590d98
Anshul Khandelwal <anshul@anshul.io>
2017-06-13 13:36:20 +0530

    Fixes style issues

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -30,7 +30,7 @@ class Grid extends PureComponent {
 
 Grid.propTypes = {
   board: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string)).isRequired,
-  onSquareClick2: PropTypes.func.isRequired,
+  onSquareClick: PropTypes.func.isRequired,
 };
 
 export default Grid;
